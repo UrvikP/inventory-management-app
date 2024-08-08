269d510ff7c6c95a37ead79273665ffb1f964c20
UrvikP <urvik95@gmail.com>
2024-08-08 19:16:21 -0400

    updated firebase.js to include process.env.KEY

--- firebase.js
+++ firebase.js
@@ -1,9 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import GOOGLE_API from './.env';
+
 
 const firebaseConfig = {
- apiKey: GOOGLE_API,
+ apiKey: process.env.GOOGLE_API,
  authDomain: "inventory-management-app-846ff.firebaseapp.com",
  projectId: "inventory-management-app-846ff",
  storageBucket: "inventory-management-app-846ff.appspot.com",
